038bee17dd4dbdb7f12dcd190fce9f2c12bda46b
ValeriaCarrillo <a259605@uach.mx>
2016-05-16 04:37:34 -0600

    Update code.js

--- js/code.js
+++ js/code.js
@@ -1,11 +1,13 @@
 $(document).ready(
 
 function(){
+	//$("#mostrar").empty();
+
 alert("ヴァラのぺーじです。\nよろしくお願いし。\nこれはアラートです。");
 
 $("#cargar").click(function(){
 
-	alert("quibo");
+	alert("Estás entrando a...");
 	$("#contenido").load("http://valeriacarrillo.github.io/");
 });
 
@@ -57,11 +59,85 @@ $("#Ajax").click(function(){
  		 });
  	 });
 
-	 $("#crear").click(function(){
+	 $("#crear").click(function() {
+		 $("#mostrar").fadeIn("slow");
+		 $("#mostrar").css("visibility","visible");
+		 $("#mostrar").css("top", "70px");
+		 $("#mostrar").css("left", "0");
+		 $("#mostrar").css("right", "0");
+		 $("#mostrar").css("margin-right", "auto");
+		 $("#mostrar").css("margin-left",  "auto");
+		 $("#mostrar").animate({
+		 marginLeft: "auto",
+		 fontSize: "14px",
+		 borderWidth: "10px",
+		 width: "900px"
+	 }, 500 );
+		 $("#cuadros").animate({
+		 margin: "235px auto"
+	 }, 500 );
+
+});
 			 /*$.post("https://andreihelo-restful-api.herokuapp.com/students", {registration_number: 259605,name: "Kitaro",last_name: "Gegege",status: "°o°"}, "json", function(result){
 		         $("span").text(JSON.stringify(result))
 		     });
 		 	*/
+$("aceptar").click(function() {
+	var matricula = $('input[id=matricula]').val();
+ var nombre = $('input[id=nombre]').val();
+ var apellido = $('input[id=apellido]').val();
+ var status = $( '#status option:selected' ).val();
+	var mensaje = "Error(es) destectado(s) \n";
+	 var ban=0;
+
+if((matricula.length === 0)||(matricula.length < 6)){
+		alert("No ingresó la matrícula");
+		if((matricula.length > 6)){
+		 alert("La matrícula debe ser de 6 dígitos.");
+		}
+
+ }
+ if(nombre.length === 0){
+	 mensaje = mensaje + "- Falta Nombre\n";
+ }
+ if(apellido.length === 0){
+	 mensaje = mensaje + "- Falta Apellido\n";
+	 }
+	 if(status  === $('#status option:first').val()){
+		mensaje = mensaje + "- Falta status\n";
+			 ban++;
+	 }
+
+	 if(mensaje.length > 26){
+			 alert(mensaje);
+	 }
+
+if((matricula.length > 0)&&(nombre.length > 0)&&(apellido.length > 0)&&(ban === 0)&&(matricula.length === 6)){
+
+		var estudiante = {
+		 "registration_number": matricula,
+		 "name": mombre,
+		 "last_name": apellido,
+		 "status": status
+		};
+
+		$.ajax({
+			 url: "https://andreihelo-restful-api.herokuapp.com/students",
+			 method: "POST",
+			 data: estudiante,
+			 success: function(result, status, xhr) {
+				 alert("Registrado con exito");
+				 abrir();
+				 //refrescar();
+		}
+});
+	 });
+
+	 $("#cerrar").click(function(){
+		 $("#cuadros").animate({
+		 margin: "70px auto"
+		 }, 500 );
+		 $("#mostrar").fadeOut( "slow" );
 	 });
 
 
